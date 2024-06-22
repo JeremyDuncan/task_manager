ee0ab60d701c5cd3568cc39b79894096725575be
JeremyDuncan <jduncan46@my.devry.edu>
2024-06-22 10:34:42 -0400

    removed old endpoints

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,10 +11,6 @@ class TasksController < ApplicationController
     end
   end
 
-  # def show
-  #   @task = current_user.tasks.find(params[:id])
-  #   render json: @task
-  # end
   def show
     respond_to do |format|
       format.json { render json: @task }
@@ -38,26 +34,17 @@ class TasksController < ApplicationController
     end
   end
 
-  # def destroy
-  #   @task = current_user.tasks.find(params[:id])
-  #
-  #   if @task.destroy
-  #     flash[:notice] = 'Task was successfully deleted.'
-  #   else
-  #     flash[:alert] = 'There was an error deleting the task.'
-  #   end
-  #
-  #   redirect_to tasks_path
-  # end
   def destroy
     @task.destroy
     head :no_content
   end
 
+
   private
   def set_task
     @task = current_user.tasks.find(params[:id])
   end
+
   def task_params
     params.require(:task).permit(:title, :description, :due_date, :category_id, tag_ids: [])
   end
