d5cddcbb0edc4732934c7cbcb76450e47621ba90
JeremyDuncan <jduncan46@my.devry.edu>
2024-06-22 08:56:32 -0400

    Updated Category Controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,38 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @categories = current_user.categories
+    render json: @categories
+  end
+
+  def create
+    @category = current_user.categories.build(category_params)
+    if @category.save
+      render json: @category, status: :created
+    else
+      render json: @category.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @category = current_user.categories.find(params[:id])
+    if @category.update(category_params)
+      render json: @category
+    else
+      render json: @category.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category = current_user.categories.find(params[:id])
+    @category.destroy
+    head :no_content
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
