512d1c6d07564359cc1875fd0f2a4d679757dff8
JeremyDuncan <jduncan46@my.devry.edu>
2024-06-22 09:59:30 -0400

    added validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
   belongs_to :user
   has_many :tasks
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { scope: :user_id, message: "You already have a category with this name" }
 end
