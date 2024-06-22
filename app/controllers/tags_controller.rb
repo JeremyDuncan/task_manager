6b0c96eff42782c74b9f781cddb1e82b24c67e6a
JeremyDuncan <jduncan46@my.devry.edu>
2024-06-22 09:01:02 -0400

    Updated  controllers

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,38 @@
 class TagsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @tags = current_user.tags
+    render json: @tags
+  end
+
+  def create
+    @tag = current_user.tags.build(tag_params)
+    if @tag.save
+      render json: @tag, status: :created
+    else
+      render json: @tag.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @tag = current_user.tags.find(params[:id])
+    if @tag.update(tag_params)
+      render json: @tag
+    else
+      render json: @tag.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @tag = current_user.tags.find(params[:id])
+    @tag.destroy
+    head :no_content
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,68 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @tasks = current_user.tasks
+    respond_to do |format|
+      format.html
+      format.json { render json: @tasks }
+    end
+  end
+
+  # def show
+  #   @task = current_user.tasks.find(params[:id])
+  #   render json: @task
+  # end
+  def show
+    respond_to do |format|
+      format.json { render json: @task }
+    end
+  end
+  def create
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      render json: @task, status: :created
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @task = current_user.tasks.find(params[:id])
+    if @task.update(task_params)
+      render json: @task
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  # def destroy
+  #   @task = current_user.tasks.find(params[:id])
+  #
+  #   if @task.destroy
+  #     flash[:notice] = 'Task was successfully deleted.'
+  #   else
+  #     flash[:alert] = 'There was an error deleting the task.'
+  #   end
+  #
+  #   redirect_to tasks_path
+  # end
+  def destroy
+    @task.destroy
+    head :no_content
+  end
+
+  private
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
+  def task_params
+    params.require(:task).permit(:title, :description, :due_date, :category_id, tag_ids: [])
+  end
 end
+
+
+
+
